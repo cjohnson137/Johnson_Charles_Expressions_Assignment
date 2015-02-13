155b1dfea73987d99ecdcd571cca62007ce5f3d1
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-12 20:20:26 -0600

    Modified Percentages
    I adjusted the percentage variables so that they have their own values
    and not calculated in the array.

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,20 @@ var waitName = prompt("What is your waiter's name?");
 var srvAmount = prompt("Please Enter Receipt Amount:");
 // var srvAmount = srvAmountPrompt;
 // var quality = prompt("Was the service poor, good, or excellent")
-var tipPercentage = [srvAmount * .05, srvAmount * .15, srvAmount * .2];
-var totalBill = [srvAmount + tipPercentage[0], srvAmount + tipPercentage[1], srvAmount + tipPercentage[2]];
+var tipPercentage = [.05, .15, .2];
+var poorTip = srvAmount * tipPercentage[0];
+var goodTIp = srvAmount * tipPercentage[1];
+var excelTip = srvAmount * tipPercentage[2];
+
+var totalTipl = [(srvAmount + tipPercentage[0]), srvAmount + tipPercentage[1], srvAmount + tipPercentage[2]];
 var fivePercent = tipPercentage[0];
-var totalBill1 = srvAmount + fivePercent;
+var totalBill = srvAmount + fivePercent;
 
 
 
 console.log(waitName + " should get a tip either of the following tips. ");
-console.log("Poor Service (5%): $" + tipPercentage[0] + " / Good Service (15%): $" + tipPercentage[1] + " / Excellent (20%): $" + tipPercentage[2]);
+console.log("Poor Service (5%): $" + poorTip + " / Good Service (15%): $" + goodTIp + " / Excellent (20%): $" + excelTip);
+console.log(totalBill[0]);
 
 // console.log("If " + waitName + " provided poor service pay $" + totalBill1);
 // console.log("If " + waitName + " provided good service pay $" + totalBill[1]);
