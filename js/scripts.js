99ca90fd77ebd8a2fff2a7a4442d3fe27f554f32
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-12 21:44:22 -0600

    Corrected Missing Semicolon
    I needed to add a semi colon to line 10

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
  */
 
 // Input Data
-var restName = prompt ("Which restaurant did you visit tonight?")
+var restName = prompt ("Which restaurant did you visit tonight?");
 var waitName = prompt("What is your waiter's name?");
 var srvAmount = prompt("Please enter the total amount of your dinner.");
 
@@ -23,7 +23,7 @@ var excelTip = srvAmount * tipPercentage[2];
 //var totalTipl = [(srvAmount + poorTip), srvAmount + goodTip, srvAmount + excelTip];
 
 // Calculate totals with dinner amount and tip
-var fivePercent = tipPercentage[0];
+// var fivePercent = tipPercentage[0];
 var poorBill = parseInt(srvAmount) + parseInt(poorTip);
 var goodBill = parseInt(srvAmount) + parseInt(goodTip);
 var excelBill = parseInt(srvAmount) + parseInt(excelTip);
